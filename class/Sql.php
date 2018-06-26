28803fa4a2a3ccfd101a8286178a7848095c4738
Bruno Moreira Alves <bmoreiraalves@gmail.com>
2018-06-26 17:06:29 -0300

    Início do Projeto

---
+++ class/Sql.php
@@ -0,0 +1,61 @@
+<?php 
+//Exibindo todos os Erros e Warnings para facilitar a identificação
+ini_set('display_errors', true);
+error_reporting(E_ALL);
+
+
+class Sql extends PDO
+{
+	private $conn;
+
+	public function __construct(){
+		// Para conectar com banco de dados MySql
+		define('BD_Mysql', ['mysql:host=localhost;', 'dbname=dbphp7', 'root', '']);
+		
+		// Para conectar com banco de dados SQL Server
+		define('BD_Sql', ['sqlsrv:Database=bdphp7;', 'server=localhost\SQLEXPRESS;ConnectionPooling=0;', 'bmalves', '21,1983,']);	
+		
+		$this->conn = new PDO(BD_Mysql[0].BD_Mysql[1], BD_Mysql[2], BD_Mysql[3]);
+
+	}
+
+	private function setParams($statment, $parameters = array()){
+
+		foreach ($parameters as $key => $value) {
+			
+			$statment->setParam($key, $value);
+
+		}
+
+	}
+
+	private function setParam($statement, $key, $value){
+
+		$statement->bindParam($key, $value);
+
+	}
+
+	public function query($rawQuery, $params = array()){
+		
+		$stmt = $this->conn->prepare($rawQuery);
+
+		$this->setParams($stmt, $params);
+
+		$stmt->execute();
+
+		return $stmt;
+
+	}
+
+	public function select($rawQuery, $params = array()):array
+	{
+
+		$stmt = $this->query($rawQuery, $params);
+
+		return $stmt->fetchALL(PDO::FETCH_ASSOC);
+
+	}
+}
+
+
+ ?>
